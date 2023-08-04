8420d2de093ddbb2d41dd64d8dac76d61fabbc30
linki <linkinoskol@gmail.com>
2023-08-04 20:50:03 +0300

    syn

--- General_stud/Prac.py
+++ General_stud/Prac.py
@@ -1,3 +1,10 @@
-a = True
-b = not a
-print(not (a and b))
+a = 1
+b = 2
+c = 3
+e = 4
+f = 5
+g = 6
+
+# True and-expressions return the result of the last operation:
+print(b + c * f >= e and (f + g) * c)  # (17 >= 4 is True) and 33 -> 33
+print((f + g) * c and b + c * f >= e)  # 33 and (17 >= 4 is True) -- > True
