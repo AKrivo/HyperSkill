19960a5bf9b2bdf006632e5049e7d92f88bf43f0
linki <linkinoskol@gmail.com>
2023-08-07 17:58:40 +0300

    syn

--- General_stud/Prac.py
+++ General_stud/Prac.py
@@ -8,3 +8,4 @@ g = 6
 # True and-expressions return the result of the last operation:
 print(b + c * f >= e and (f + g) * c)  # (17 >= 4 is True) and 33 -> 33
 print((f + g) * c and b + c * f >= e)  # 33 and (17 >= 4 is True) -- > True
+
