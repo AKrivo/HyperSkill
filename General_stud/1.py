ec5b29637d8db23e68037d86ac5dac2cabc220b4
linki <linkinoskol@gmail.com>
2023-12-10 14:58:25 +0100

    syn

--- General_stud/1.py
+++ General_stud/1.py
@@ -1,5 +1,39 @@
-import random
+# Waveguide calc
+import math as m
 
-random.seed(52)
 
-print(random.choice([1, 2, 3]))
+class Waveguide():
+    def __init__(self, w, h, epsi, freq):
+        self.w = w * 1e-3  # width in m
+        self.h = h * 1e-3  # height in m
+        self.epsi = epsi  # relative permittivity
+        self.freq = freq * 1e9  # Frequency in GHz
+        self.c = 299792458  # c in m/s
+        self.mu = 1
+
+    def getCutoff(self):
+        Fc = self.c / (m.sqrt(self.epsi * self.mu) * 2 * self.w)
+
+        return Fc * 1e-9
+
+    def getWavelength(self):
+        # lg = (2 * self.w)/(m.sqrt(4 * self.freq**2 * self.epsi * self.w**2 -1))
+        lf = self.c / (self.freq * m.sqrt(self.epsi))
+        lcof = 2 * self.w
+        lg = lf / m.sqrt(1 - (lf / lcof) ** 2)
+        return lg
+
+
+WR10 = Waveguide(2.54, 1.27, 1, 90e9)
+
+WR275 = Waveguide(0.864, 0.432, 1, 325e9)
+WSi275 = Waveguide(0.2, 0.15, 11.9, 275e9)
+WSi = Waveguide(0.65, 0.3, 11.9, 90e9)
+# print(WR10.getCutoff())
+# print(WSi.getCutoff())
+# print(WR10.getWavelength())
+print(WSi.getWavelength())
+# print(WSi275.getWavelength())
+# print(WSi275.getCutoff())
+print(WSi275.getCutoff())
+print(WSi.getCutoff())
--- General_stud/Test.py
+++ General_stud/Test.py
@@ -1,24 +1,9 @@
-mystring = "01010010010001010100100101001001101000110010101010101111010010010110100101101010100110101010101010001110011"
-triads = ["000", "001", "010", "011", "100", "101", "110", "111"]
-k = 0
-results = dict.fromkeys(triads, [0, 0])
-while k < len(triads):
-    x = 0
-    y = 0
-    i = 0
-    newstring = mystring
-    t = len(newstring)
-    while i < t - 3:
-        if newstring[0:3] == triads[k]:
-            if newstring[3] == "0":
-                x += 1
-                results[triads[k]] = [x, y]
-            if newstring[3] == "1":
-                y += 1
-                results[triads[k]] = [x, y]
-        newstring = newstring[1:]
-        i += 1
-    k += 1
-    print(results)
-for i in results:
-    print(str(i) + ": " + str(",".join([str(item)for item in results[i]])))
+number = 70e9
+
+# Determine the exponent in the scientific notation
+exponent = int(str(number).split("e")[1])
+
+# Convert the number to a string in the desired format
+formatted_number = "{:.0f} * 10^{}".format(number / 10**exponent, exponent)
+
+print(formatted_number)
